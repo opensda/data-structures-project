1d522c56ad42714e9eab4178d6cb845fcff236ff
Sudarev_D <89365265993@mail.ru>
2023-06-22 15:37:25 +0300

    stack and tests mofified

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,36 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Node, Stack
+
+class TestArrs(unittest.TestCase):
+    def test_Node(self):
+        n1 = Node(1)
+        self.assertEqual(n1.data, 1)
+        self.assertEqual(n1.next_node, None)
+        # self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+
+    def test_Stack(self):
+        stack = Stack()
+        self.assertEqual(stack.top, None)
+
+
+
+    # def test_push(self):
+    #     stack1 = Stack()
+    #     stack1.push('data1')
+    #     stack1.push('data2')
+    #     stack1.push('data3')
+    #     self.assertEqual(stack1.top.data, 'data3')
+    #     self.assertEqual(stack1.top.next_node.data, 'data2')
+    #     self.assertEqual(stack1.top.next_node.next_node.data, 'data1')
+
+
+
+
+
+
+
+
+
+
+
