3160c25fc9c7e17d736e4b6d5a03229c5a964aef
Sudarev_D <89365265993@mail.ru>
2023-06-25 21:33:53 +0700

    stack and tests mofified in according task2

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -7,7 +7,7 @@ class TestArrs(unittest.TestCase):
         n1 = Node(1)
         self.assertEqual(n1.data, 1)
         self.assertEqual(n1.next_node, None)
-        # self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+
 
     def test_Stack(self):
         stack = Stack()
@@ -15,16 +15,21 @@ class TestArrs(unittest.TestCase):
 
 
 
-    # def test_push(self):
-    #     stack1 = Stack()
-    #     stack1.push('data1')
-    #     stack1.push('data2')
-    #     stack1.push('data3')
-    #     self.assertEqual(stack1.top.data, 'data3')
-    #     self.assertEqual(stack1.top.next_node.data, 'data2')
-    #     self.assertEqual(stack1.top.next_node.next_node.data, 'data1')
+    def test_push(self):
+        stack1 = Stack()
+        stack1.push('data1')
+        stack1.push('data2')
+        stack1.push('data3')
+        self.assertEqual(stack1.top.data, 'data3')
+        self.assertEqual(stack1.top.next_node.data, 'data2')
+        self.assertEqual(stack1.top.next_node.next_node.data, 'data1')
 
-
+    def test_pop(self):
+        stack2 = Stack()
+        stack2.push('data111')
+        stack2.push('data222')
+        lst_remvd = stack2.pop()
+        self.assertEqual(lst_remvd, 'data222')
 
 
 
